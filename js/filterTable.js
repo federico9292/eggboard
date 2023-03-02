0a6e4a5e43a1f8155a0b05a995c568d397f0efc1
federico <federico.999@live.it>
2023-03-02 22:06:03 +0100

    changes

--- js/filterTable.js
+++ js/filterTable.js
@@ -1,10 +1,8 @@
-let func = new Function (
-    $(document).ready(function(){
-        $("#myInput").on("keyup", function() {
-          var value = $(this).val().toLowerCase();
-          $("#myTable tr").filter(function() {
+$(document).ready(function () {
+    $("#myInput").on("keyup", function () {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-          });
         });
-      })
-    );
+    });
+})

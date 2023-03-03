19359f9100e5510dbd9086d896631a7c6fd1fb59
federico <federico.999@live.it>
2023-03-03 17:03:37 +0100

    fix buttons

--- js/filterTable.js
+++ js/filterTable.js
@@ -8,6 +8,11 @@ $(document).ready(function () {
     createDropdowns();
 })
 
+var _rows = 10;
+var _iterations = 0;
+var _current_item;
+var _current_name;
+
 function submitID() {
     alert("The form was submitted");
 }
@@ -15,14 +20,23 @@ function submitID() {
 function getLeaderboard(type, name) {
     const http = new XMLHttpRequest()
     //http.open("GET", "https://egg-brosssh.vercel.app/getLeaderboard?element=" + name + "&n=10&top_n=1")
-    http.open("GET", "https://egg-brosssh-nh5u9iyas-brosssh.vercel.app/getLeaderboard?element=" + name + "&n=10&top_n=1");
+    if (window._current_item &&(window._current_item.toLowerCase() != type.toLowerCase() || window._current_name.toLowerCase() != name.toLowerCase())) {
+        window._rows = 10;
+        window._iterations = 0;
+    } 
+    http.open("GET", "https://egg-brosssh-nh5u9iyas-brosssh.vercel.app/getLeaderboard?element=" + name + "&n=" + window._rows + "&top_n=1");
     //http.setRequestHeader("Access-Control-Allow-Origin","*");
     http.send();
     name = name.charAt(0).toUpperCase() + name.slice(1)
+    window._current_item = type;
+    window._current_name = name;
     http.onload = () => {
+        displayButtonMore();
         displaySearch();
+
         if (type == 'Artifact' || type == 'Stone') {
             fillTableArtifact(http.responseText, name, type);
+
         }
         else {
             fillTableIngredient(http.responseText, name);
@@ -31,7 +45,7 @@ function getLeaderboard(type, name) {
 }
 
 function fillTableIngredient(response, name) {
-    console.log(response);
+    //console.log(response);
     jQuery('.jumbotron h1')[0].textContent = 'Eggboard | Ingredient | ' + name;
     jQuery('#thead tr').remove(); //to clear the columns;
     jQuery('#myTable tr').remove(); //To clear the rows (pointed by @nunners)
@@ -63,8 +77,8 @@ function fillTableIngredient(response, name) {
 }
 
 function fillTableArtifact(response, name, type) {
-    console.log(response);
-    jQuery('.jumbotron h1')[0].textContent = 'Eggboard | ' + type +' | ' + name;
+    //console.log(response);
+    jQuery('.jumbotron h1')[0].textContent = 'Eggboard | ' + type + ' | ' + name;
 
     jQuery('#thead tr').remove(); //to clear the columns;
     jQuery('#myTable tr').remove(); //To clear the rows (pointed by @nunners)
@@ -99,7 +113,7 @@ function fillTableArtifact(response, name, type) {
 function displaySearch() {
 
     jQuery('#myInput')[0].classList.remove('hidden');
-    jQuery('#myInput')[0].value='';
+    jQuery('#myInput')[0].value = '';
     jQuery('#myInput')[0].placeHolder = 'Type something to search the table';
 
 }
@@ -111,24 +125,24 @@ function createDropdowns() {
     var arrayIngredients = "gold, tau, titanium".split(", ");
     var arrayStones = "clarity, lunar, prophecy, life, quantum, dilithium, soul, terra, tachyon, shell".split(", ");
     var arrayArtifacts = "bob, light, deflector, siab, actuator, monocle, metronome, feather, chalice, compass, rainstick, beak, lens, medallion, gusset, ankh, brooch, vial, necklace, totem, cube".split(", ");
-    
-    
+
+
     arrayIngredients.forEach(element => {
         var a = document.createElement('a');
-        a.setAttribute('class','dropdown-item');
-        a.setAttribute('href','#');
-        a.setAttribute('role',"button");
-        a.setAttribute("onclick","getLeaderboard(\'ingredient\',\'"+element+"\')");
+        a.setAttribute('class', 'dropdown-item');
+        a.setAttribute('href', '#');
+        a.setAttribute('role', "button");
+        a.setAttribute("onclick", "getLeaderboard(\'ingredient\',\'" + element + "\')");
         a.text = element.charAt(0).toUpperCase() + element.slice(1);
-        menu_ingredients.appendChild(document.createElement('li')).appendChild(a); 
+        menu_ingredients.appendChild(document.createElement('li')).appendChild(a);
     });
-    
+
     arrayStones.forEach(element => {
         var a = document.createElement('a');
-        a.setAttribute('class','dropdown-item');
-        a.setAttribute('href','#');
-        a.setAttribute('role',"button");
-        a.setAttribute("onclick","getLeaderboard(\'Stone\',\'"+element+"\')"); //Stones are like Artifacts, with 4 Tiers
+        a.setAttribute('class', 'dropdown-item');
+        a.setAttribute('href', '#');
+        a.setAttribute('role', "button");
+        a.setAttribute("onclick", "getLeaderboard(\'Stone\',\'" + element + "\')"); //Stones are like Artifacts, with 4 Tiers
         a.text = element.charAt(0).toUpperCase() + element.slice(1);
         menu_stones.appendChild(document.createElement('li')).appendChild(a);
 
@@ -136,13 +150,43 @@ function createDropdowns() {
 
     arrayArtifacts.forEach(element => {
         var a = document.createElement('a');
-        a.setAttribute('class','dropdown-item');
-        a.setAttribute('href','#');
-        a.setAttribute('role',"button");
-        a.setAttribute("onclick","getLeaderboard(\'Artifact\',\'"+element+"\')");
+        a.setAttribute('class', 'dropdown-item');
+        a.setAttribute('href', '#');
+        a.setAttribute('role', "button");
+        a.setAttribute("onclick", "getLeaderboard(\'Artifact\',\'" + element + "\')");
         a.text = element.charAt(0).toUpperCase() + element.slice(1);
-        menu_artifacts.appendChild(document.createElement('li')).appendChild(a); 
+        menu_artifacts.appendChild(document.createElement('li')).appendChild(a);
 
-    });    
+    });
 
+}
+
+function addMoreRows() {
+    window._iterations = ++window._iterations;
+    switch ( window._iterations) {
+        case 1:
+            window._rows =  window._rows + 10;
+            break;
+        case 2:
+            window._rows =  window._rows+ 25;
+            break;
+        default:
+            window._rows =  window._rows+ 50;
+            break;
+    }
+}
+
+function displayButtonMore(params) {
+    //se iterazione è diverso da 0 mostro il bottone
+    var button = jQuery('#buttonMore');
+   
+        button[0].classList.remove('hidden');
+        button[0].setAttribute('onclick','getMoreRows("'+window._current_item+'","'+window._current_name+'")');
+        
+    
+}
+function getMoreRows(current_item,current_name) {
+    addMoreRows();
+    console.log(window._rows,'rows');
+    getLeaderboard(current_item,current_name.toLowerCase());
 }
